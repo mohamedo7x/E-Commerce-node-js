108bc38c58406255e239c566f438e7798376aacb
mohamedo7x <mohamedo7x@hotmail.com>
2023-04-25 02:37:27 +0200

    Fix error userRoute

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,6 @@ router.get('/' ,user);
 router.post('/register' , register);
 router.post('/login' , login);
 router.post('/login' , logout);
-router.route('/update').put('/:id' , updateUser);
+router.put('/update/:id' , updateUser);
 
 export default router;
