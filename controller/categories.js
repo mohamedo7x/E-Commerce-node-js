072c5c801916f070deb8d6b9ec867ed3a433a9f6
mohamedo7x <mohamedo7x@hotmail.com>
2023-05-06 18:11:43 +0200

    Create CRUD fro categories

--- controller/categories.js
+++ controller/categories.js
@@ -1,28 +1,157 @@
 import CategoriesShchema from "../schema/CategoriesShchema.js";
+import user from '../schema/userSchema.js';
 import asyncWrapper from '../middleware/async.js';
 import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 
+const createCat = asyncWrapper(async (req, res) => {
+
+    /* Check If Token exisit and if he is admin */
+    let token = req.cookies.access_token;
+    const data = await jwt.verify(token, process.env.SECRET_KEY);
+    if (!token) return res.status(400).json({
+        message: 'Pleas login'
+    });
+    if (!data.isAdmin) return res.status(400).json({
+        message: "Admin only"
+    });
+
+    /* Check iF there same cat */
+    const {
+        name,
+        img
+    } = req.body; // there no img wright now .... fix it later
+    const Data_cat = await CategoriesShchema.findOne({
+        name
+    }, {
+        __v: 0,
+    });
 
 
+    if (Data_cat) return res.status(400).json({
+        message: "Categorie alredy exisit ",
+        Data_cat
+    });
 
-const createCat = asyncWrapper(async(req , res)=> {
-        
-  
+    /* Create Cat */
+
+    const create = await new CategoriesShchema({
+        name,
+        img
+    });
+    await create.save();
+    res.status(201).json({
+        message: "Categorie has been created",
+        info: [
+            create
+        ]
+    })
 
 });
 
-const updateCat = asyncWrapper(async(req , res)=> {
+const updateCat = asyncWrapper(async (req, res) => {
+    /* Check If User Exisit and privilage */
+    let token_h = req.cookies.access_token;
+    if (!token_h) return res.status(400).json({
+        message: "Pleas Loign"
+    });
+    let token = await jwt.verify(token_h, process.env.SECRET_KEY);
+    if (!token.isAdmin) return res.status(400).json({
+        message: "this bage for ADMIN ONLY "
+    })
+
+
+
+    let cat = req.params.name;
+    cat = cat.toString();
+
+    /* Secarch In Cat First  */
+    let Data = await CategoriesShchema.findOne({
+        name: cat
+    });
+
+    if (!Data) return res.status(404).json({
+        message: "Categories Not Found"
+    });
+
+    /* Change Name  */
+    const DataName = req.body.name;
+    let UP_cat = await CategoriesShchema.updateOne({
+        name: cat
+    }, {
+        name: DataName
+    })
+
+    res.status(200).json({
+        message: "Categorie Name Changed"
+    });
+
 
 });
 
-const deleteCat = asyncWrapper(async(req , res)=> {
+const deleteCat = asyncWrapper(async (req, res) => {
+    let token_h = req.cookies.access_token;
+    if (!token_h) return res.status(400).json({
+        message: "Pleas Loign"
+    });
+    let token = await jwt.verify(token_h, process.env.SECRET_KEY);
+    if (!token.isAdmin) return res.status(400).json({
+        message: "this bage for ADMIN ONLY "
+    })
+
+    let cat = req.params.name;
+    cat = cat.toString();
+    if(!cat) return res.status(400).json({message : "Enter Name Of Categories"})
+     /* Secarch In Cat First  */
+     let Data = await CategoriesShchema.findOne({
+        name: cat
+    });
+
+    if (!Data) return res.status(404).json({
+        message: "Categories Not Found"
+    });
+
+    const delteCat =await CategoriesShchema.deleteOne({name : cat});
+
+        res.status(200).json({message: 'Categories deleted'})
 
 });
 
-const showCat = asyncWrapper(async (req ,res)=> {
+const showCat = asyncWrapper(async (req, res) => {
+    /* Check If User Exisit and privilage */
+    let token_h = req.cookies.access_token;
+    if (!token_h) return res.status(400).json({
+        message: "Pleas Loign"
+    });
+    let token = await jwt.verify(token_h, process.env.SECRET_KEY);
+    if (!token.isAdmin) return res.status(400).json({
+        message: "this bage for ADMIN ONLY "
+    })
+    const name = req.body.name;
+    const Data = await CategoriesShchema.findOne({
+        name
+    });
+
+    res.status(200).json({
+        ALERT: "Categorie",
+        info: [
+            Data
+        ]
+    })
 
 });
 
-
-export  {showCat , createCat , updateCat , deleteCat};
+const showCatAll = asyncWrapper(async(req , res)=> {
+    const All_cat =await CategoriesShchema.find({} , {createdAt : 0 , updatedAt:0 , __v:0 , _id:0});
+    res.status(200).json({All_cat})
+})
+export {
+    showCat,
+    showCatAll,
+    createCat,
+    updateCat,
+    deleteCat
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,12 @@
 import Cat from '../schema/CategoriesShchema.js';
-import {showCat , createCat , updateCat , deleteCat} from '../controller/categories.js'
+import {showCat , createCat , updateCat , deleteCat , showCatAll} from '../controller/categories.js'
 import express from 'express';
 const route = express.Router();
 
-
-
-
+route.get('/all' , showCatAll)
+route.get('/' ,showCat );
+route.post('/create' ,createCat);
+route.put('/update/:name' , updateCat )
+route.delete('/delete/:name' , deleteCat)
 
 export default route;
