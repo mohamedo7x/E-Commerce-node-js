f808cb4a9df51c1d4c7beba97e7ad7981c9949e4
mohamedo7x <mohamedo7x@hotmail.com>
2023-05-07 01:14:22 +0200

    Create sendMailer

---
+++ util/sendMail.js
@@ -0,0 +1,32 @@
+import nodeMailer from 'nodemailer';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const sendMail = async(option) =>{
+    const transport = nodeMailer.createTransport({
+        host : process.env.MAILER_HOST,
+        port : process.env.MAILER_PORT,
+        secure: true,
+        auth: {
+            user: process.env.MAILER_USER,
+            pass:process.env.MAILER_PASS
+        }
+    })
+
+
+
+
+
+    const emailOTP = {
+        from : "E-commerce",
+        to:option.email,
+        subject : option.subject,
+        html : option.message,
+    }
+
+
+
+    await transport.sendMail(emailOTP);
+};
+
+
